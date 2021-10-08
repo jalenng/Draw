5a72ed8a68464622f339a938b8718dcae73a6647
someguy13 <ryan.kwong@gmail.com>
2021-10-07 22:45:59 -0700

    Update Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,10 +8,14 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb2d;
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
+    [SerializeField] private float movementSmoothing = 0.05f;
     private bool onGround;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float overlap;
     [SerializeField] private LayerMask ground;
+    private Vector3 m_Velocity = Vector3.zero;
+    private float horizontal;
+    private bool jump;
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
@@ -19,22 +23,32 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        rb2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal")*speed, rb2d.velocity.y);
-        onGround = Physics2D.OverlapCircle(groundCheck.position, overlap, ground);
-        Jump();
-
-    }
-    
-    private void Jump()
-    {
+        horizontal = Input.GetAxisRaw("Horizontal");
         if (Input.GetButtonDown("Jump"))
         {
-            // Debug.Log("Button Pressed: " + doublejump);
-            if (onGround)
-            {
-                // Debug.Log("Ground Jump");
-                rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
-            }
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        if (jump) Jump();
+        else onGround = Physics2D.OverlapCircle(groundCheck.position, overlap, ground);
+        jump = false;
+    }
+
+
+    private void Move()
+    {
+        Vector3 target = new Vector2(horizontal * speed, rb2d.velocity.y);
+        rb2d.velocity = Vector3.SmoothDamp(rb2d.velocity, target, ref m_Velocity, movementSmoothing);
+    }
+    private void Jump()
+    {
+        if (onGround && jump)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
         }
     }
 }
