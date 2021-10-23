9193501640daa405a0865bb949b3e1d671c6044f
jalenng <jalen_ng@outlook.com>
2021-10-22 23:14:12 -0700

    Begin implementation of level transition and level end trigger

---
+++ Assets/Scripts/LevelEndTrigger.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEndTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) {
+        FindObjectOfType<LevelManager>().LoadNextScene();
+    }
+}
