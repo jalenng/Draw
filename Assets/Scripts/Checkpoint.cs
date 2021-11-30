7895b0283d187fe70c64d79c960def4a137d7836
jalenng <jalen_ng@outlook.com>
2021-11-29 21:24:37 -0800

    Update checkpoint prefab with activation animation

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,20 +4,37 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    // Configuration parameters
     [Tooltip("How much to offset from the checkpoint's position (bottom of pole) when respawning the player")]
     [SerializeField] Vector3 respawnOffset = new Vector3(0.5f, 1.25f, 0.0f);
+
+    // State variables
     bool activated = false;
+
+    // Cached component references
+    Animator anim;
+
+    private void Start() 
+    {
+        anim = GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
+        // If the checkpoint has not been activated,
+        // and the player has collided with the checkpoint...
         if (!activated && other.CompareTag("Player")) 
         {
             PlayerMovement playerMovement = other.GetComponent<PlayerMovement>();
 
+            // Update the player's respawn position
             if (playerMovement) 
             {
                 playerMovement.SetRespawnPos(transform.position + respawnOffset);
                 activated = true;
+
+                // Animate the checkpoint activation
+                anim.SetBool("Activated", true);
             }
             else
                 Debug.LogError("Cannot set checkpoint because PlayerController was not found");
