a65123f21fe1fbd3b66e83f415f5151fdb20e4c2
jalenng <jalen_ng@outlook.com>
2022-02-11 21:56:25 -0800

    Update prefabs; do more organization

--- Assets/Scripts/LevelEndTrigger.cs
+++ Assets/Scripts/LevelEndTrigger.cs
@@ -6,6 +6,7 @@ using UnityEngine.Events;
 public class LevelEndTrigger : MonoBehaviour
 {
     [SerializeField] UnityEvent onLevelEnd;
+
     private void OnTriggerEnter2D(Collider2D other) {
         onLevelEnd.Invoke();
     }
