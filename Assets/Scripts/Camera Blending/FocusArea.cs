fb23fe4116329123bcd8451943214e92940ec928
jalenng <jalen_ng@outlook.com>
2022-09-13 00:38:22 -0700

    Update vignettes, level instructions, and Claire death anim

--- Assets/Scripts/Camera Blending/FocusArea.cs
+++ Assets/Scripts/Camera Blending/FocusArea.cs
@@ -11,6 +11,7 @@ public class FocusArea : MonoBehaviour
     // Configurable parameters
     [SerializeField] [Tooltip("How far to zoom the camera relative to the focus area's size")] [Range(0, 2)]
     float zoomFactor = 0.8f;
+    bool isLocked = false;
 
     // Cached object references
     CameraBlender blender;
@@ -20,24 +21,44 @@ public class FocusArea : MonoBehaviour
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
+        if (isLocked)
+            return;
         if(other.CompareTag("Player")) {
-            // Unity camera size is 1/2 of its height
-            float camSizeByHeight = transform.lossyScale.y / 2;
-            float camSizeByWidth = transform.lossyScale.x / 3;  // 3:2 ratio
-            float camSize = Mathf.Max(camSizeByHeight, camSizeByWidth) / zoomFactor;
-            
-            blender.Focus(transform.position, camSize);
+            Focus();
         }
     }
 
     private void OnTriggerExit2D(Collider2D other) {
-        if(other.CompareTag("Player")) {
-            blender.Unfocus();
-        }
+        if (isLocked)
+            return;
+        if(other.CompareTag("Player"))
+            Unfocus();
+    }
+
+    private void Focus() {
+        // Unity camera size is 1/2 of its height
+        float camSizeByHeight = transform.lossyScale.y / 2;
+        float camSizeByWidth = transform.lossyScale.x / 3;  // 3:2 ratio
+        float camSize = Mathf.Max(camSizeByHeight, camSizeByWidth) / zoomFactor;
+        
+        blender.Focus(transform.position, camSize);
+    }
+
+    private void Unfocus() {
+        blender.Unfocus();
     }
 
     public void UpdateArea(int x)
     {
         transform.localScale = new Vector3(x, transform.localScale.y, transform.localScale.z);
+    }
+
+    public void SetLock(bool locked)
+    {
+        isLocked = locked;
+        if (isLocked)
+            Focus();
+        else
+            Unfocus();
     }
 }
