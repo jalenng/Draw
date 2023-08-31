94d7814d365fd6aa24f8125a753b147a1ca68ede
Jay <42555186+jalenng@users.noreply.github.com>
2023-08-30 21:16:09 -0700

    add fullscreen mode and resolution picker in settings (#120)

---
+++ Assets/Scripts/UI/Components/UIButtonScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIButtonScript : MonoBehaviour
+{
+    // Configuration parameters
+    [SerializeField] private Color enabledFGColor = Color.black;
+    [SerializeField] private Color disabledFGColor = new Color(0.75f, 0.75f, 0.75f, 1f);
+
+    // Cached components
+    [SerializeField] private GameObject textObject;
+
+    private TextMeshProUGUI tmp;
+    private Button button;
+    private Outline outline;
+
+    private void Start()
+    {
+        tmp = textObject.GetComponent<TextMeshProUGUI>();
+        button = GetComponent<Button>();
+        outline = GetComponent<Outline>();
+    }
+
+    private void Update()
+    {
+        // Get the disabled state of the button
+        bool isDisabled = !button.interactable;
+        Color fgColor = isDisabled ? disabledFGColor : enabledFGColor;
+
+        // Set text color and outline color based on the state
+        tmp.color = fgColor;
+        outline.effectColor = fgColor;
+    }
+}
--- Assets/Scripts/UI/Settings Menu/AudioSettings.cs
+++ Assets/Scripts/UI/Settings Menu/AudioSettings.cs
@@ -3,25 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
-using TMPro;
 
-public class SettingsMenu : MonoBehaviour
+public class AudioSettings : MonoBehaviour
 {
     // Audio mixer
     [Header("Audio Mixer")]
     [SerializeField] AudioMixer audioMixer;
     [SerializeField] AudioMixerParamsConfig mixerParams;
-    // Sliders
-    [Header("Sliders")]
+
+    [Header("Option widgets")]
     public Slider masterVolumeSlider;
     public Slider BGMVolumeSlider;
     public Slider SFXVolumeSlider;
-
-    // Object references
-    [SerializeField] private GameObject versionTMPObj;
-    [SerializeField] private MenuManager menuHolder;
-
-    SceneLoader levelLoader;
 
     // Helper function to logarithmically map (0, 1) to a decibel value used for the audio mixer attenuation.
     float RatioToDecibel(float ratio)
@@ -39,19 +32,6 @@ public class SettingsMenu : MonoBehaviour
 
     void Start()
     {
-        levelLoader = FindObjectOfType<SceneLoader>();
-
-        // Update version number text
-        TextMeshProUGUI versionTMP = versionTMPObj?.GetComponent<TextMeshProUGUI>();
-        if (versionTMP)
-        {
-            versionTMP.text = $"v{Application.version}";
-            if (Debug.isDebugBuild)
-            {
-                versionTMP.text += "-debug";
-            }
-        }
-
         // Set sliders to current values of the audio mixer
         float masterVolume;
         audioMixer.GetFloat(mixerParams.masterVolume, out masterVolume);
@@ -64,32 +44,24 @@ public class SettingsMenu : MonoBehaviour
         float SFXVolume;
         audioMixer.GetFloat(mixerParams.SFXVolume, out SFXVolume);
         SFXVolumeSlider.value = DecibelToRatio(SFXVolume);
+
     }
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            menuHolder.enableSettings(false);
-        }
-    }
+
     public void OnMasterVolumeChanged()
     {
+        Debug.Log($"[SettingsMenu] Setting master volume to {mixerParams.masterVolume}");
         audioMixer.SetFloat(mixerParams.masterVolume, RatioToDecibel(masterVolumeSlider.value));
-
     }
 
     public void OnBGMVolumeChanged()
     {
+        Debug.Log($"[SettingsMenu] Setting BGM volume to {mixerParams.BGMVolume}");
         audioMixer.SetFloat(mixerParams.BGMVolume, RatioToDecibel(BGMVolumeSlider.value));
     }
 
     public void OnSFXVolumeChanged()
     {
+        Debug.Log($"[SettingsMenu] Setting SFX volume to {mixerParams.SFXVolume}");
         audioMixer.SetFloat(mixerParams.SFXVolume, RatioToDecibel(SFXVolumeSlider.value));
     }
-    public void ReturnToMenu()
-    {
-        levelLoader.LoadMainMenu();
-    }
-
 }
