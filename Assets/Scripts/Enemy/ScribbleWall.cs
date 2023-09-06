c0e1454704475c4ba296e8bb12ecda24a682c99b
spicywheatbread <jonathanluc2010@yahoo.com>
2023-09-06 00:37:18 -0700

    updateScribbleWall
    - Moved a copy of enemy.cs into scribble wall's script. I didn't want to create a new bool as a condition for Move() for all enemies, so I just made ScribbleWall.CS a copy of enemy that has a bool for Move() in Update(). So, we can set it to move or not when the player is dead. (
        - ScribbleWall will stop when player dies to prevent it from touching any respawn points it shouldn't
        - I worked on this at 1AM, so maybe in hindsight it won't be the easiest solution to this, but I don't see why not
            - Tried setting constraints on RigidBody2D, but Move() sets the position directly.
            - Tried altering Rb2d simulated false/true, didn't work
        - Enemy.cs removed from scribblewall
        - Made a prefab for scribblewall
    
    - Scribble wall points have been changed
        - There are two types of points
            - RespawnPoints, which will be set on top of checkpoints. They detect collision with the ScribbleWall and set its respawn point.
                - These respawn points will be set directly on checkpoint flags, so that the wall will only set a new respawn point when it directly touches a checkpoint, by which point the player should have touched it already. (Make sure all flags are mandatory to touch, to avoid a player running past a flag and softlocking themselves
            - PathPoints, which ScribbleWall just uses to determine its path through the level.
        - These are separate because the path between each flag isn't always the movement we want the wall to take
    
    - For some reason, there was a PauseMenu in the enemy folder. Deleted that
    - Grouped normal enemies and their points under new folder
    - Renamed checkpoints for ease of knowing their order

--- Assets/Scripts/Enemy/ScribbleWall.cs
+++ Assets/Scripts/Enemy/ScribbleWall.cs
@@ -2,17 +2,53 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random=UnityEngine.Random;
 
 public class ScribbleWall : RespawnInterface
 {
-    public Vector3 respawnPos;
-    public List<GameObject> touchedPoints;
+    [SerializeField] Rigidbody2D rb2d;
+    [SerializeField] private float initWait;
+    [SerializeField] private float speed;
 
+    [Header("Straight Movement")]
+    [SerializeField] private List<Transform> points = new List<Transform>();
+    [SerializeField] private float distanceOffset;
+    
+    private int index = 0;
+    private float wait;
+    private Animator anim;
+    Vector3 respawnPos;
+    bool playerDead = false;
     void Awake()
     {
         respawnPos = transform.position;
+        Animator anim = GetComponent<Animator>();
+        AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo(0);//could replace 0 by any other animation layer index
+        anim.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
     }
-
+    void Update() {
+        if(!playerDead) MoveStraight();
+    }
+    private void MoveStraight() 
+    {
+        if (points.Count > 0)
+        {
+            transform.position = Vector2.MoveTowards(
+                transform.position,
+                points[index].transform.position,
+                speed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, points[index].transform.position) < distanceOffset)
+            {
+                if (wait <= 0)
+                {
+                    index = (index + 1) % points.Count;
+                    wait = initWait;
+                }
+                else
+                    wait -= Time.deltaTime;
+            }
+        }
+    }
     public void SetRespawnPos(Vector3 pos) 
     {
         respawnPos = pos;
@@ -21,6 +57,8 @@ public class ScribbleWall : RespawnInterface
     {
         if(other.gameObject.CompareTag("Player"))
         {
+            other.gameObject.GetComponent<PlayerMovement>().Die();
+            playerDead = true;
             StartCoroutine(Respawn(1f));
         }
     }
@@ -31,5 +69,6 @@ public class ScribbleWall : RespawnInterface
     {
         yield return new WaitForSeconds(wait);
         transform.position = respawnPos;
+        playerDead = false;
     }
 }
