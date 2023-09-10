aea9a0d69e31eb48fd487460a9932b8b3c69c644
spicywheatbread <105267517+spicywheatbread@users.noreply.github.com>
2023-09-10 01:55:20 -0700

    updateScribbleWall (#143)
    * updateScribbleWall
    
    - Moved a copy of enemy.cs into scribble wall's script. I didn't want to create a new bool as a condition for Move() for all enemies, so I just made ScribbleWall.CS a copy of enemy that has a bool for Move() in Update(). So, we can set it to move or not when the player is dead. (
        - ScribbleWall will stop when player dies to prevent it from touching any respawn points it shouldn't
        - I worked on this at 1AM, so maybe in hindsight it won't be the easiest solution to this, but I don't see why not
            - Tried setting constraints on RigidBody2D, but Move() sets the position directly.
            - Tried altering Rb2d simulated false/true, didn't work
        - Enemy.cs removed from scribblewall
        - Made a prefab for scribblewall
    
    - Scribble wall points have been changed
        - There are two types of points
            - RespawnPoints, which will be set on top of checkpoints. They detect collision with the ScribbleWall and set its respawn point.
                - These respawn points will be set directly on checkpoint flags, so that the wall will only set a new respawn point when it directly touches a checkpoint, by which point the player should have touched it already. (Make sure all flags are mandatory to touch, to avoid a player running past a flag and softlocking themselves
            - PathPoints, which ScribbleWall just uses to determine its path through the level.
        - These are separate because the path between each flag isn't always the movement we want the wall to take
    
    - For some reason, there was a PauseMenu in the enemy folder. Deleted that
    - Grouped normal enemies and their points under new folder
    - Renamed checkpoints for ease of knowing their order
    
    * Respawn Logic Update
    
    - Chase Level update is a copy of chase level from before the editor update so we can move changes over
    - ScribbleWall no longer uses points to respawn, but will calculate the direction of the previous point in its path, and move a fixed amount backwards.
    
    * Moved stickman
    
    woohoo
    
    * Delete Chase Update
    
    Deleted the Chase Update Level, not needed. However, there's a copy on Jonathan's desktop just in case....

--- Assets/Scripts/Enemy/ScribbleWall.cs
+++ Assets/Scripts/Enemy/ScribbleWall.cs
@@ -2,17 +2,23 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random=UnityEngine.Random;
 
-public class ScribbleWall : RespawnInterface
+public class ScribbleWall : Enemy
 {
-    public Vector3 respawnPos;
-    public List<GameObject> touchedPoints;
+    [SerializeField] Rigidbody2D rb2d;
 
+    [SerializeField] private float respawnOffset;
+
+    Vector3 respawnPos;
+    bool playerDead = false;
     void Awake()
     {
         respawnPos = transform.position;
     }
-
+    void Update() {
+        if(!playerDead) MoveStraight();
+    }
     public void SetRespawnPos(Vector3 pos) 
     {
         respawnPos = pos;
@@ -21,15 +27,21 @@ public class ScribbleWall : RespawnInterface
     {
         if(other.gameObject.CompareTag("Player"))
         {
+            other.gameObject.GetComponent<PlayerMovement>().Die();
+            playerDead = true;
             StartCoroutine(Respawn(1f));
         }
     }
-    public override void StartRespawn() {
-        StartCoroutine(Respawn(0f));
+    public void StartRespawn() {
+        StartCoroutine(Respawn(1f));
     }
     IEnumerator Respawn(float wait)
     {
         yield return new WaitForSeconds(wait);
-        transform.position = respawnPos;
+        Vector3 direction = transform.position - points[index - 1].transform.position;
+        direction = direction.normalized;
+        direction *= respawnOffset;
+        transform.position -= direction;
+        playerDead = false;
     }
 }
