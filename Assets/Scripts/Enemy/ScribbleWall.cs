f676caa3d02ecdcd6084b30735e5652f1db9373d
spicywheatbread <jonathanluc2010@yahoo.com>
2023-09-22 00:29:49 -0700

    More respawn logic!!
    - Removed unnecessary scripts from old scribble wall logic
    - Deleted respawn points in chase level
    - scribble wall now factors in player's distance from their respawn to make sure it spawns in behind them (needs testing)

--- Assets/Scripts/Enemy/ScribbleWall.cs
+++ Assets/Scripts/Enemy/ScribbleWall.cs
@@ -7,26 +7,21 @@ using Random=UnityEngine.Random;
 public class ScribbleWall : Enemy
 {
     [SerializeField] Rigidbody2D rb2d;
-
     [SerializeField] private float respawnOffset;
-
-    Vector3 respawnPos;
+    [SerializeField] private PlayerMovement playerMovement;
     bool playerDead = false;
-    void Awake()
-    {
-        respawnPos = transform.position;
-    }
+    Vector3 playerPos;
+
     void Update() {
+        // Only move if player isn't dead
         if(!playerDead) MoveStraight();
-    }
-    public void SetRespawnPos(Vector3 pos) 
-    {
-        respawnPos = pos;
     }
     private void OnCollisionEnter2D(Collision2D other) 
     {
+        // If collider into player, kill it and disable movement, respawn wall
         if(other.gameObject.CompareTag("Player"))
         {
+            playerPos = other.transform.position;
             other.gameObject.GetComponent<PlayerMovement>().Die();
             playerDead = true;
             StartCoroutine(Respawn(1f));
@@ -38,10 +33,16 @@ public class ScribbleWall : Enemy
     IEnumerator Respawn(float wait)
     {
         yield return new WaitForSeconds(wait);
+        // Get direction of previous point in path
         Vector3 direction = transform.position - points[index - 1].transform.position;
         direction = direction.normalized;
-        direction *= respawnOffset;
+        // Find distance of player to last respawn point to add to offset
+        float distToRespawn = Vector3.Distance(playerPos, playerMovement.respawnPos);
+        // Create Vector3 with magnitude of distance to respawnpoint plus our offset
+        direction *= (respawnOffset + distToRespawn);
+        Debug.Log(direction);
         transform.position -= direction;
+        Debug.Log("Respawning at " + transform.position);
         playerDead = false;
     }
 }
