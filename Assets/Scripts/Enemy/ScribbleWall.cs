58067b03a68e340db503005fb314d525b3a76f09
jalenng <jalen_ng@outlook.com>
2023-09-30 14:26:54 -0700

    use new sprite for scribble wall + add lint on vscode save

--- Assets/Scripts/Enemy/ScribbleWall.cs
+++ Assets/Scripts/Enemy/ScribbleWall.cs
@@ -2,33 +2,58 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Random=UnityEngine.Random;
+using Random = UnityEngine.Random;
 
+[ExecuteAlways]
 public class ScribbleWall : Enemy
 {
     [SerializeField] private PlayerMovement playerMovement;
     [SerializeField] private float speedUpDistance = 40;
     [SerializeField] private float baseSpeed;
     private bool respawning = false;
-    void FixedUpdate() {
+
+    void Update()
+    {
+        // Iterate through the points and connect them with a line in the editor.
+        // This makes it easier to see the path that the scribble wall will take.
+        if (points.Count > 0)
+        {
+            Vector3 pos1 = points[0].position;
+            for (int i = 1; i < points.Count; i++)
+            {
+                Vector3 pos2 = points[i].position;
+                Debug.DrawLine(pos1, pos2, Color.red);
+                pos1 = pos2;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
         // If we're respawning, don't do anything.
         UpdateSpeed();
-        if(!respawning) {
+        if (!respawning)
+        {
             // If the player is dead & wall is not respawning, respawn this
-            if(playerMovement.GetPlayerDead()) {
+            if (playerMovement.GetPlayerDead())
+            {
                 respawning = true;
-                StartRespawn(); 
-            } else {
-            // Otherwise, move forward
+                StartRespawn();
+            }
+            else
+            {
+                // Otherwise, move forward
                 MoveStraight();
             }
         }
     }
-    void Awake() {
+    void Awake()
+    {
         baseSpeed = speed;
     }
     // Vector3(-45,121.220001,0) OG Starting Position 
-    public void StartRespawn() {
+    public void StartRespawn()
+    {
         StartCoroutine(Respawn(1f));
     }
     IEnumerator Respawn(float wait)
@@ -37,20 +62,25 @@ public class ScribbleWall : Enemy
         transform.position = originalPosition;
         respawning = false;
     }
-    public void setRespawnPosition(Vector3 pos) {
+    public void setRespawnPosition(Vector3 pos)
+    {
         originalPosition = pos;
     }
-    private void UpdateSpeed() {
+    private void UpdateSpeed()
+    {
         float distanceFromPlayer = Vector3.Distance(transform.position, playerMovement.transform.position);
         Debug.Log("distance: " + distanceFromPlayer);
-        if(distanceFromPlayer > speedUpDistance) {
+        if (distanceFromPlayer > speedUpDistance)
+        {
             // Find how far away from "speedUpDistance" the wall is
             distanceFromPlayer -= speedUpDistance;
             // Divide by number to make smaller ;p
             // Add exponential number to speed based on this number
             distanceFromPlayer = distanceFromPlayer * 10;
             this.speed = baseSpeed + 5 + distanceFromPlayer;
-        } else {
+        }
+        else
+        {
             // If the wall gets within the threshhold, set speed back to the original speed.
             this.speed = baseSpeed;
         }
