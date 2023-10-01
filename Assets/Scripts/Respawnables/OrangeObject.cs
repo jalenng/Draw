57f4a3a587481b11df0a3488c839e97b9df8c58f
Jonathan Luc <jonathanluc2010@yahoo.com>
2023-10-01 15:32:28 -0700

    Remove unnecessary function
    Title

--- Assets/Scripts/Respawnables/OrangeObject.cs
+++ Assets/Scripts/Respawnables/OrangeObject.cs
@@ -55,7 +55,4 @@ public class OrangeObject : RespawnInterface
         transform.position = respawnPos;
         transform.rotation = respawnRotation;
     }
-    public void SetBodyType(RigidbodyType2D bodyType) {
-        rb2d.bodyType = bodyType;
-    }
 }
