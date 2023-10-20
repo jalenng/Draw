a55a8c89be54ee3625fbb91b32c9f175e88a4ea7
Jay <42555186+jalenng@users.noreply.github.com>
2023-10-20 00:33:16 -0700

    Begin work on localization (#262)
    * begin work on localization
    
    * add more localization
    
    * update ui to be responsive to text size/length
    
    * set up system for dialogue localization + test dialog objs
    
    * rebind claire dialogue
    
    * rebind mike dialogue
    
    * rebind nathan dialogue
    
    * add player prefs locale selector
    
    * update gitignore

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -1,13 +1,33 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+[System.Serializable]
+public class DialogueEntry
+{
+    public Sprite avatar;
+
+    // Content-related
+    [TextArea(5, 5)]
+    public string content;
+
+    [Tooltip("Check this box to use the CPS below. Otherwise, will use the default.")]
+    public bool useCPS;
+
+    [Range(0, 120)]
+    [Tooltip("Characters to reveal per second. Set this to 0 to disable the typewriting effect")]
+    public int CPS = 30;
+
+}
 
 [CreateAssetMenu(fileName = "Dialogue", menuName = "Dialogue", order = 1)]
 public class Dialogue : ScriptableObject
 {
     // Default parameters
     [Header("Defaults")]
-    [Range(0, 120)] [Tooltip("Set to 0 to disable typewriting effect")] 
+    [Range(0, 120)]
+    [Tooltip("Set to 0 to disable typewriting effect")]
     public int CPS = 30;
     public string SFXDirectory = "";
 
---
+++ Assets/Scripts/UI/LanguageSelector.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+using TMPro;
+
+public class LanguageSelector : MonoBehaviour
+{
+    [SerializeField] GameObject template;
+    [SerializeField] GameObject footer;
+
+    void Start()
+    {
+        // Instantiate the buttons in the dialogue.
+        List<Locale> locales = LocalizationSettings.AvailableLocales.Locales;
+        foreach (Locale l in locales)
+        {
+            GameObject newButton = Instantiate(template, footer.transform) as GameObject;
+            newButton.SetActive(true);
+            newButton.GetComponentInChildren<TextMeshProUGUI>().text = l.LocaleName;
+            newButton.GetComponentInChildren<Button>().onClick.AddListener(() => SetLocale(l));
+        }
+    }
+
+    private void SetLocale(Locale l)
+    {
+        string localeName = l.LocaleName;
+        Debug.Log($"[LanguageSelector] Setting locale to {localeName}");
+        LocalizationSettings.SelectedLocale = l;
+    }
+}
